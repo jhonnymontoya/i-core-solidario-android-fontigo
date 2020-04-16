5cb5d28a61451864e6f75a75072ad4dbb4dd0b5d
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-04-16 12:25:19 -0500

    Se inicia el desarrollo del Dashboard

--- app/src/main/java/com/startlinesoft/icore/solidario/android/ais/DashBoardActivity.java
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/DashBoardActivity.java
@@ -2,13 +2,53 @@ package com.startlinesoft.icore.solidario.android.ais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class DashBoardActivity extends AppCompatActivity {
+import com.startlinesoft.icore.solidario.ApiClient;
+import com.startlinesoft.icore.solidario.ApiException;
+import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreApiClient;
+import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreAppCompatActivity;
+import com.startlinesoft.icore.solidario.api.LoginApi;
+import com.startlinesoft.icore.solidario.api.models.LoginInfo;
+
+public class DashBoardActivity extends ICoreAppCompatActivity implements View.OnClickListener {
+
+    private Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        this.boton = (Button)this.findViewById(R.id.btnSalir);
+        this.boton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        this.verificarRed();
+
+        ApiClient cliente = ICoreApiClient.getApiClient();
+        LoginApi loginApi = new LoginApi(cliente);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    loginApi.logout();
+                } catch (ApiException e) {
+                }
+                boton.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent i = new Intent(getBaseContext(), LoginActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                });
+            }
+        }).start();
     }
 }
