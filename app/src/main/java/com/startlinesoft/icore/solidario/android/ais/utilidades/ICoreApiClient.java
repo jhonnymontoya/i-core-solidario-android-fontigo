df96a67c28dd49275b3bebb5b56993142ead4f06
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-04-16 12:23:25 -0500

    Se adiciona funcionalidad para saber si el token de seguridad está activo, se adiciona funcionalidad que el pedir ApiClient contenga el token de seguridad

--- app/src/main/java/com/startlinesoft/icore/solidario/android/ais/utilidades/ICoreApiClient.java
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/utilidades/ICoreApiClient.java
@@ -1,9 +1,13 @@
 package com.startlinesoft.icore.solidario.android.ais.utilidades;
 
 import android.os.Build;
+import android.webkit.WebView;
 
 import com.startlinesoft.icore.solidario.ApiClient;
+import com.startlinesoft.icore.solidario.ApiException;
 import com.startlinesoft.icore.solidario.Configuration;
+import com.startlinesoft.icore.solidario.api.LoginApi;
+import com.startlinesoft.icore.solidario.auth.HttpBearerAuth;
 
 public class ICoreApiClient {
 
@@ -13,21 +17,43 @@ public class ICoreApiClient {
         ApiClient cliente = Configuration.getDefaultApiClient();
         //cliente.setBasePath("https://test.i-core.co/api");
         cliente.setUserAgent(ICoreApiClient.getFirmaDevice());
+
+        if(ICoreApiClient.token.length() > 0) {
+            HttpBearerAuth auth = (HttpBearerAuth) cliente.getAuthentication("icore_auth");
+            auth.setBearerToken(ICoreApiClient.token);
+        }
         return cliente;
     }
 
+    /**
+     * Establece el token de autenticación
+     * @param token
+     */
     public static void setToken(String token){
         ICoreApiClient.token = token;
     }
 
+    /**
+     * Verifica si el token de autenticación es o no válido y vigente
+     * @return
+     */
+    public static boolean esTokenValido() {
+        LoginApi loginApi = new LoginApi(ICoreApiClient.getApiClient());
+        ICoreApiClient.token = "";
+        try {
+            loginApi.ping();
+            return true;
+        } catch (ApiException e) {
+            if(e.getCode() == 401) {
+                return false;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
     private static String getFirmaDevice() {
-        return new StringBuilder(Build.DEVICE)
-                .append(";")
-                .append(Build.MODEL)
-                .append(";")
-                .append(Build.PRODUCT)
-                .append(";")
-                .append(Build.VERSION.RELEASE)
-                .toString();
+        return System.getProperty("http.agent");
     }
 }
