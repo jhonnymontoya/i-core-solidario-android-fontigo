ed27d6c65e2e743db2d3659dd014f93c5900c318
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-04-21 18:51:02 -0500

    Se agrega funcionalidad para menú inferior

--- app/src/main/java/com/startlinesoft/icore/solidario/android/ais/DashBoardActivity.java
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/DashBoardActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -31,7 +32,7 @@ import com.startlinesoft.icore.solidario.api.models.LoginInfo;
 import com.startlinesoft.icore.solidario.api.models.Recaudo;
 import com.startlinesoft.icore.solidario.api.models.Socio;
 
-public class DashBoardActivity extends ICoreAppCompatActivity implements View.OnClickListener {
+public class DashBoardActivity extends ICoreAppCompatActivity implements View.OnClickListener, MenuItem.OnMenuItemClickListener {
 
     private ActivityDashBoardBinding bnd;
     private SocioViewModel socioViewModel;
@@ -83,10 +84,58 @@ public class DashBoardActivity extends ICoreAppCompatActivity implements View.On
 
             }
         });
+
+        bnd.cvAhorros.setOnClickListener(this);
+        bnd.cvCreditos.setOnClickListener(this);
+        bnd.cvRecaudos.setOnClickListener(this);
+
+        // Barra de navegación inferior
+        bnd.bnvMenu.getMenu().setGroupCheckable(0, true, false);
+        for(int i = 0;  i< bnd.bnvMenu.getMenu().size(); i++) {
+            bnd.bnvMenu.getMenu().getItem(i).setChecked(true);
+            bnd.bnvMenu.getMenu().getItem(i).setOnMenuItemClickListener(this);
+        }
     }
 
     @Override
     public void onClick(View v) {
+        // Ir a Ahorros
+        if(v.equals(bnd.cvAhorros)) {
+            Toast.makeText(this, "Click en tarjeta de ahorros", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Ir a Créditos
+        if(v.equals(bnd.cvCreditos)) {
+            Toast.makeText(this, "Click en tarjeta de créditos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Ir a Recaudos
+        if(v.equals(bnd.cvRecaudos)) {
+            Toast.makeText(this, "Click en tarjeta de recaudos", Toast.LENGTH_SHORT).show();
+            return;
+        }
         //this.logout();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        final int DOCUMENTACION = 0;
+        final int SIMULADOR_CREDITO = 1;
+        final int SOLICITAR_CREDITO = 2;
+        if(item.equals(bnd.bnvMenu.getMenu().getItem(DOCUMENTACION))) {
+            Toast.makeText(this, "Click documentación", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(item.equals(bnd.bnvMenu.getMenu().getItem(SIMULADOR_CREDITO))) {
+            Toast.makeText(this, "Click Simulador de crédito", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(item.equals(bnd.bnvMenu.getMenu().getItem(SOLICITAR_CREDITO))) {
+            Toast.makeText(this, "Click solicitud de crédito", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return false;
     }
 }
