a250ec1e7a4ecf098f9526d0a04518ccbc952e17
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-04-21 17:25:29 -0500

    Se implementa el socio view model y factory

---
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/models/SocioViewModel.java
@@ -0,0 +1,33 @@
+package com.startlinesoft.icore.solidario.android.ais.models;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.startlinesoft.icore.solidario.ApiClient;
+import com.startlinesoft.icore.solidario.ApiException;
+import com.startlinesoft.icore.solidario.api.SocioApi;
+import com.startlinesoft.icore.solidario.api.models.Socio;
+
+public class SocioViewModel extends ViewModel {
+
+    private MutableLiveData<Socio> socio;
+
+    public SocioViewModel(ApiClient cliente) {
+        socio = new MutableLiveData<Socio>();
+        SocioApi socioApi = new SocioApi(cliente);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socio.postValue(socioApi.socio());;
+                } catch (ApiException e) {
+                }
+            }
+        }).start();
+    }
+
+    public LiveData<Socio> getSocio() {
+        return socio;
+    }
+}
