510db2c33dff91b1502a23645fde502734897136
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-05-04 09:36:43 -0500

    Se implementa la actividad de detalle ahorros

--- app/src/main/java/com/startlinesoft/icore/solidario/android/ais/DetalleAhorroActivity.java
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/DetalleAhorroActivity.java
@@ -1,16 +1,20 @@
 package com.startlinesoft.icore.solidario.android.ais;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.startlinesoft.icore.solidario.android.ais.adapters.adapters.AhorroDetalleAdapter;
 import com.startlinesoft.icore.solidario.android.ais.databinding.ActivityDetalleAhorroBinding;
 import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreAppCompatActivity;
+import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreGeneral;
 import com.startlinesoft.icore.solidario.api.models.DetalleAhorro;
-import com.startlinesoft.icore.solidario.api.models.Socio;
+import com.startlinesoft.icore.solidario.api.models.MovimientoAhorro;
+
+import java.util.List;
 
 public class DetalleAhorroActivity extends ICoreAppCompatActivity implements View.OnClickListener {
 
@@ -22,6 +26,29 @@ public class DetalleAhorroActivity extends ICoreAppCompatActivity implements Vie
         super.onCreate(savedInstanceState);
         bnd = ActivityDetalleAhorroBinding.inflate(getLayoutInflater());
         setContentView(bnd.getRoot());
+
+        //Se valida token activo
+        this.validarLogin();
+
+        detalleAhorro = (DetalleAhorro) getIntent().getSerializableExtra("AHORRO");
+
+        setSupportActionBar(bnd.tbToolbar);
+        bnd.tbToolbar.setNavigationIcon(R.drawable.ic_angle_left);
+        bnd.tbToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        bnd.ivImagen.setImageBitmap(ICoreGeneral.getSocioImagen());
+        bnd.ivImagen.setOnClickListener(this);
+
+        bnd.tvNombreModalidad.setText(detalleAhorro.getNombreModalidad());
+
+        bnd.rvAhorrosDealle.setLayoutManager(new LinearLayoutManager(this));
+        List<MovimientoAhorro> movimientosAhorros = detalleAhorro.getMovimientos();
+        bnd.rvAhorrosDealle.setAdapter(new AhorroDetalleAdapter(movimientosAhorros));
     }
 
     @Override
@@ -39,5 +66,4 @@ public class DetalleAhorroActivity extends ICoreAppCompatActivity implements Vie
             return;
         }
     }
-
 }
