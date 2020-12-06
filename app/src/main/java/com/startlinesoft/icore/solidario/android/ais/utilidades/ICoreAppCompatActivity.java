78248a9fcd23ec34224acd34e2bcec85e406319d
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-12-06 13:15:05 -0500

    Se adelanta la clase ICoreAppCompatActivity
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- app/src/main/java/com/startlinesoft/icore/solidario/android/ais/utilidades/ICoreAppCompatActivity.java
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/utilidades/ICoreAppCompatActivity.java
@@ -1,6 +1,30 @@
 package com.startlinesoft.icore.solidario.android.ais.utilidades;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ICoreAppCompatActivity extends AppCompatActivity {
+
+    private static final String TOKEN = "TOKEN";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+
+    /**
+     * Remueve el título de la barra ToolBar
+     */
+    protected void removerTituloBarra() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+    }
 }
