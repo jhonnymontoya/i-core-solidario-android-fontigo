05b5f38ede8bd3bff870c0a824190669787cbaf1
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-12-06 01:23:09 -0500

    Se crea clase padre activity

---
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/utilidades/ICoreAppCompatActivity.java
@@ -0,0 +1,6 @@
+package com.startlinesoft.icore.solidario.android.ais.utilidades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ICoreAppCompatActivity extends AppCompatActivity {
+}
