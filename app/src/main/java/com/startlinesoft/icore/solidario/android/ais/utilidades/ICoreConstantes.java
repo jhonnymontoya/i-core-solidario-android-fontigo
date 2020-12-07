acc9a6c106e44e9e1956c074c98853013c2a4600
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-12-07 08:46:00 -0500

    Se crea clase para almacenar constantes
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

---
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/utilidades/ICoreConstantes.java
@@ -0,0 +1,6 @@
+package com.startlinesoft.icore.solidario.android.ais.utilidades;
+
+public class ICoreConstantes {
+    public static final String TOKEN = "TOKEN";
+    public static final String PREFERENCE_VIBRADOR = "vibrador";
+}
