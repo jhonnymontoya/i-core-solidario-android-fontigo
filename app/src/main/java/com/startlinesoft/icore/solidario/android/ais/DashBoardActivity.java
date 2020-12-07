5c31afb81a5335fc06f6ec6932bec719141919fa
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-12-06 20:54:35 -0500

    Se agrega DashBoardActivity
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

---
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/DashBoardActivity.java
@@ -0,0 +1,14 @@
+package com.startlinesoft.icore.solidario.android.ais;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
