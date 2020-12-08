cf753fdec1df7546c73d21b68bf43373881882c8
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-12-07 20:30:26 -0500

    Se agrega constante sobre el proyecto en ejecución
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- app/src/main/java/com/startlinesoft/icore/solidario/android/ais/utilidades/ICoreConstantes.java
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/utilidades/ICoreConstantes.java
@@ -2,5 +2,6 @@ package com.startlinesoft.icore.solidario.android.ais.utilidades;
 
 public class ICoreConstantes {
     public static final String TOKEN = "TOKEN";
+    public static final String PROYECTO = "test";
     public static final String PREFERENCE_VIBRADOR = "vibrador";
 }
