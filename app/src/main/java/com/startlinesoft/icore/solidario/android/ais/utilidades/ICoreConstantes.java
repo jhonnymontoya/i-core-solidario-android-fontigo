2c0a91f7ce3d8f71b7d99786a91bc3ea01364762
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-12-07 21:21:10 -0500

    Se agrega constante para el touchid
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- app/src/main/java/com/startlinesoft/icore/solidario/android/ais/utilidades/ICoreConstantes.java
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/utilidades/ICoreConstantes.java
@@ -4,4 +4,5 @@ public class ICoreConstantes {
     public static final String TOKEN = "TOKEN";
     public static final String PROYECTO = "test";
     public static final String PREFERENCE_VIBRADOR = "vibrador";
+    public static final String PREFERENCE_TOUCHID = "touchid";
 }
