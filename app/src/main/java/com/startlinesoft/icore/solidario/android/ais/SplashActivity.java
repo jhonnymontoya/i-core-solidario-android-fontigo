a42f0a01d3c6e29507a78208b2774c0b489e1388
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-12-07 20:33:15 -0500

    Se termina la implementación de la lógica del splash screen
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- app/src/main/java/com/startlinesoft/icore/solidario/android/ais/SplashActivity.java
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/SplashActivity.java
@@ -1,12 +1,8 @@
 package com.startlinesoft.icore.solidario.android.ais;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.Toast;
 
 import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreApiClient;
 import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreAppCompatActivity;
@@ -21,34 +17,28 @@ public class SplashActivity extends ICoreAppCompatActivity {
         new Handler().postDelayed(tarea, 1000);
     }
 
-    private final Runnable tarea = new Runnable() {
-        @Override
-        public void run() {
-            verificarRed();
+    private final Runnable tarea = () -> {
+        verificarRed();
 
-            if (isSetToken()) {
-                ICoreApiClient.setToken(getToken());
-                boolean esTokenValido = ICoreApiClient.esTokenValido();
-                Intent i;
-                if(esTokenValido){
-                    //Sí es válido, ir a dashboard
-                    i = new Intent(getBaseContext(), DashBoardActivity.class);
-                }
-                else{
-                    //No es válido, ir al login
-                    i = new Intent(getBaseContext(), LoginActivity.class);
-                    removerToken();
-                }
-                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(i);
-                finish();
+        if (isSetToken()) {
+            ICoreApiClient.setToken(getToken());
+            Intent i;
+            if (ICoreApiClient.esTokenValido()) {
+                //Sí es válido, ir a dashboard
+                i = new Intent(getBaseContext(), DashBoardActivity.class);
             } else {
-                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
-                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                //No es válido, ir al login
+                i = new Intent(getBaseContext(), LoginActivity.class);
                 removerToken();
-                startActivity(i);
-                finish();
             }
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i);
+        } else {
+            Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            removerToken();
+            startActivity(i);
         }
+        finish();
     };
 }
