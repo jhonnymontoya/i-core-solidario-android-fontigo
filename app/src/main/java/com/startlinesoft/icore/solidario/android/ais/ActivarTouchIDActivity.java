5bbfce178826aa3d0b98278d2746d2d318cf2662
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-12-12 00:27:41 -0500

    Se agrega actividad para ingresar y activar touch id
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

---
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/ActivarTouchIDActivity.java
@@ -0,0 +1,14 @@
+package com.startlinesoft.icore.solidario.android.ais;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivarTouchIDActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activartouchid);
+    }
+}
