970ffcecbad137a6c8eece2fd68d8485cd769900
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-12-13 21:39:17 -0500

    Se avanza con la implementación de la actividad del perfil
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- app/src/main/java/com/startlinesoft/icore/solidario/android/ais/PerfilActivity.java
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/PerfilActivity.java
@@ -4,11 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PerfilActivity extends AppCompatActivity {
+import com.startlinesoft.icore.solidario.android.ais.databinding.ActivityPerfilBinding;
+import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreAppCompatActivity;
+import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreGeneral;
+import com.startlinesoft.icore.solidario.api.models.Socio;
+
+public class PerfilActivity extends ICoreAppCompatActivity {
+
+    private ActivityPerfilBinding bnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_perfil);
+        this.bnd = ActivityPerfilBinding.inflate(this.getLayoutInflater());
+        this.setContentView(this.bnd.getRoot());
+
+        //Se valida token activo
+        this.validarLogin();
+
+        Socio socio = ICoreGeneral.getSocio();
+        String identificacion = String.format("%s %s", socio.getTipoIdentificacion(), socio.getIdentificacion());
+
+        setSupportActionBar(bnd.tbToolbar);
+        this.getSupportActionBar().setTitle(R.string.perfil);
+        bnd.tbToolbar.setNavigationIcon(R.drawable.ic_cerrar);
+        bnd.tbToolbar.setNavigationOnClickListener(v -> this.finish());
+
+        bnd.ivImagen.setImageBitmap(ICoreGeneral.getSocioImagen());
+        bnd.tvNombre.setText(socio.getNombre());
+        bnd.tvIdentificacion.setText(identificacion);
+        bnd.tvNombreEntidad.setText(socio.getEntidad());
     }
 }
