8015496a5dcf86929d47e2ba890a79a4cce694d7
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-12-16 23:00:23 -0500

    Se agrega código para mostrar los beneficiarios
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- app/src/main/java/com/startlinesoft/icore/solidario/android/ais/BeneficiariosActivity.java
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/BeneficiariosActivity.java
@@ -1,14 +1,75 @@
 package com.startlinesoft.icore.solidario.android.ais;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class BeneficiariosActivity extends AppCompatActivity {
+import com.startlinesoft.icore.solidario.android.ais.adapters.adapters.BeneficiarioAdapter;
+import com.startlinesoft.icore.solidario.android.ais.databinding.ActivityBeneficiariosBinding;
+import com.startlinesoft.icore.solidario.android.ais.models.BeneficiarioViewModel;
+import com.startlinesoft.icore.solidario.android.ais.models.BeneficiarioViewModelFactory;
+import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreAppCompatActivity;
+import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreGeneral;
+
+public class BeneficiariosActivity extends ICoreAppCompatActivity implements View.OnClickListener {
+
+    private ActivityBeneficiariosBinding bnd;
+    private BeneficiarioViewModel beneficiarioViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_beneficiarios);
+        this.bnd = ActivityBeneficiariosBinding.inflate(this.getLayoutInflater());
+        this.setContentView(this.bnd.getRoot());
+
+        //Se valida token activo
+        this.validarLogin();
+
+        this.beneficiarioViewModel = new ViewModelProvider(this.getViewModelStore(), new BeneficiarioViewModelFactory())
+                .get(BeneficiarioViewModel.class);
+
+        this.bnd.progressBar.setVisibility(View.VISIBLE);
+        this.beneficiarioViewModel.getBeneficiarios().observe(this, beneficiarios -> {
+            this.bnd.progressBar.setVisibility(View.GONE);
+
+            if(beneficiarios.size() > 0){
+                this.bnd.rvBeneficiarios.setLayoutManager(new LinearLayoutManager(this));
+                BeneficiarioAdapter ba = new BeneficiarioAdapter(beneficiarios);
+                this.bnd.rvBeneficiarios.setAdapter(ba);
+            }
+            else{
+                this.bnd.tvBeneficiariosSinRegistros.setVisibility(View.VISIBLE);
+            }
+        });
+
+        this.setSupportActionBar(this.bnd.tbToolbar);
+        this.bnd.tbToolbar.setNavigationIcon(R.drawable.ic_angle_left);
+        this.bnd.tbToolbar.setNavigationOnClickListener(v -> this.finish());
+
+        this.bnd.ivImagen.setImageBitmap(ICoreGeneral.getSocioImagen());
+        this.bnd.ivImagen.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        this.bnd.tbToolbar.setTitle(getString(R.string.beneficiarios));
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+
+        // Ir a info de cuenta
+        if (v.equals(bnd.ivImagen)) {
+            Intent i = new Intent(this, InfoActivity.class);
+            startActivity(i);
+            return;
+        }
     }
 }
