4253d86c75343ef22f520cc5323cdcbf9e0a7d79
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-12-21 12:11:09 -0500

    Se implementa el about activity
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- app/src/main/java/com/startlinesoft/icore/solidario/android/ais/AboutActivity.java
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/AboutActivity.java
@@ -1,7 +1,12 @@
 package com.startlinesoft.icore.solidario.android.ais;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -17,10 +22,48 @@ public class AboutActivity extends ICoreAppCompatActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         this.bnd =ActivityAboutBinding.inflate(this.getLayoutInflater());
         this.setContentView(this.bnd.getRoot());
+
+        String version = "0.0.0";
+        try {
+            PackageInfo pInfo = this.getApplicationContext().getPackageManager().getPackageInfo(this.getApplicationContext().getPackageName(), 0);
+            version = pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+
+        String nombreVerion = "I-Core %s %s";
+        nombreVerion = String.format(
+                nombreVerion,
+                this.getString(R.string.app_name),
+                version
+        );
+
+        this.bnd.tvNombreVersion.setText(nombreVerion);
+
+        this.bnd.tvStartLineSoft.setText(this.getText(R.string.powered_by));
+        this.bnd.btnCancelar.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        this.removerTituloBarra();
     }
 
     @Override
     public void onClick(View v) {
         super.onClick(v);
+
+        if(v.equals(this.bnd.tvStartLineSoft)){
+            String url = "https://startlinesoft.com";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+            return;
+        }
+
+        if(v.equals(this.bnd.btnCancelar)){
+            this.finish();
+            return;
+        }
     }
 }
