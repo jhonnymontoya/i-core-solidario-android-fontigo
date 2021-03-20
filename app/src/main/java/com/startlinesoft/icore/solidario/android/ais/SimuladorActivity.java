4250f7b455b9e378f4d54ef2e1875fe7bfcee8fa
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2021-03-19 19:23:04 -0500

    Se avanza con el simulador de crédito
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- app/src/main/java/com/startlinesoft/icore/solidario/android/ais/SimuladorActivity.java
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/SimuladorActivity.java
@@ -1,10 +1,14 @@
 package com.startlinesoft.icore.solidario.android.ais;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.Nullable;
+
 import com.startlinesoft.icore.solidario.android.ais.databinding.ActivitySimuladorBinding;
 import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreAppCompatActivity;
+import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreGeneral;
 
 public class SimuladorActivity extends ICoreAppCompatActivity implements View.OnClickListener {
 
@@ -15,5 +19,33 @@ public class SimuladorActivity extends ICoreAppCompatActivity implements View.On
         super.onCreate(savedInstanceState);
         this.bnd = ActivitySimuladorBinding.inflate(this.getLayoutInflater());
         this.setContentView(this.bnd.getRoot());
+
+        //Se valida token activo
+        this.validarLogin();
+
+        this.setSupportActionBar(this.bnd.tbToolbar);
+        this.bnd.tbToolbar.setNavigationIcon(R.drawable.ic_angle_left);
+        this.bnd.tbToolbar.setNavigationOnClickListener(v -> this.finish());
+
+        this.bnd.ivImagen.setImageBitmap(ICoreGeneral.getSocioImagen());
+        this.bnd.ivImagen.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        this.bnd.tbToolbar.setTitle(getString(R.string.accion_simulador));
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+
+        // Ir a info de cuenta
+        if (v.equals(bnd.ivImagen)) {
+            Intent i = new Intent(this, InfoActivity.class);
+            startActivity(i);
+            return;
+        }
     }
 }
