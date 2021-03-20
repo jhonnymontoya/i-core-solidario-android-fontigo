acabab63af7c3cf14ff44670bf55f477c55ab23a
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2021-03-19 21:11:54 -0500

    Se prepara la interfaz del simulador
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- app/src/main/java/com/startlinesoft/icore/solidario/android/ais/SimuladorActivity.java
+++ app/src/main/java/com/startlinesoft/icore/solidario/android/ais/SimuladorActivity.java
@@ -3,16 +3,29 @@ package com.startlinesoft.icore.solidario.android.ais;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
 
 import androidx.annotation.Nullable;
 
+import com.startlinesoft.icore.solidario.ApiClient;
+import com.startlinesoft.icore.solidario.ApiException;
 import com.startlinesoft.icore.solidario.android.ais.databinding.ActivitySimuladorBinding;
+import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreApiClient;
 import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreAppCompatActivity;
 import com.startlinesoft.icore.solidario.android.ais.utilidades.ICoreGeneral;
+import com.startlinesoft.icore.solidario.api.CreditosApi;
+import com.startlinesoft.icore.solidario.api.models.ModalidadCredito;
 
-public class SimuladorActivity extends ICoreAppCompatActivity implements View.OnClickListener {
+import java.util.List;
+
+public class SimuladorActivity extends ICoreAppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
     private ActivitySimuladorBinding bnd;
+    private CreditosApi creditosApi;
+    private List<ModalidadCredito> modalidades;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +42,43 @@ public class SimuladorActivity extends ICoreAppCompatActivity implements View.On
 
         this.bnd.ivImagen.setImageBitmap(ICoreGeneral.getSocioImagen());
         this.bnd.ivImagen.setOnClickListener(this);
+
+        this.bnd.progressBar.setVisibility(View.VISIBLE);
+
+        ApiClient cliente = ICoreApiClient.getApiClient();
+        this.creditosApi = new CreditosApi(cliente);
+
+        this.bnd.modalidadCredito.setOnItemClickListener(this);
+
     }
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         this.bnd.tbToolbar.setTitle(getString(R.string.accion_simulador));
+
+        new Thread(() -> {
+            try {
+                this.modalidades = this.creditosApi.obtenerModalidadesDisponibles();
+                this.bnd.progressBar.post(() -> {
+                    this.bnd.progressBar.setVisibility(View.GONE);
+
+                    if(this.modalidades.size() > 0){
+                        String[] modalidades = this.modalidades
+                                .stream()
+                                .map(m -> m.getNombre())
+                                .toArray(String[]::new);
+
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, modalidades);
+                        this.bnd.modalidadCredito.setAdapter(adapter);
+                        this.bnd.modalidadCredito.setText(adapter.getItem(0), false);
+
+                        this.cargarPeriodicidades(this.modalidades.get(0));
+                    }
+                });
+            } catch (ApiException ignored) {
+            }
+        }).start();
     }
 
     @Override
@@ -47,5 +91,22 @@ public class SimuladorActivity extends ICoreAppCompatActivity implements View.On
             startActivity(i);
             return;
         }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        this.cargarPeriodicidades(this.modalidades.get(position));
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(this, item + " ; " + this.modalidades.get(position).getNombre(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void cargarPeriodicidades(ModalidadCredito modalidadCredito){
+        String[] periodicidades = modalidadCredito.getPeriodicidadesDePago()
+                .stream()
+                .map(p -> p.toString())
+                .toArray(String[]::new);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, periodicidades);
+        this.bnd.periodicidad.setAdapter(adapter);
+        this.bnd.periodicidad.setText(adapter.getItem(0), false);
     }
 }
